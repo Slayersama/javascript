e21e906d809b57749684284b418b77aa62ed20aa
dipanshu <79101095+Slayersama@users.noreply.github.com>
2025-09-03 20:01:20 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,34 @@
+"use strict"; //this treats all JS code as newer version
+/* 
+ Data Types In JavaScript
+    1. numbers => it has a range of 2 to the power of 53
+    2. bigInt => used to store numbers larger numbers
+    3. string => always use "" instead of ''. stores alphabetic values
+    4. boolean => contains true or false values. 
+    5. null => stand alone value.
+    6. undefined => value is not defined yet. It's not same as null both are different. We prefer this if we want to reserve variable but not the data type for future use.
+    7. symbol => unique values 
+
+
+    
+    
+    */
+   
+   
+   
+   
+   
+   let userName = "Dipanshu"; //string datatype contains only alphabets.
+   
+   let userAge = 23; //Numbers datatype contains numeric values.
+   
+   let isLoggedIn = true; // Boolean datatypes contains true or false values.
+   
+   
+   console.log(typeof null); //It is an object  in JS
+   console.log(typeof undefined); //It is a type in JS
+   console.log(typeof userName); // String
+   console.log(typeof userAge); //Number
+   console.log(typeof isLoggedIn); //Boolean 
+   
+   
