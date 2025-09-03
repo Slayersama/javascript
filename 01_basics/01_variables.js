0b4c0ed494fb9b63e89b5f0b92bd9afd43993b18
dipanshu <79101095+Slayersama@users.noreply.github.com>
2025-09-03 19:32:35 +0000

    variables done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "dipanshurn03#gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+
+// We can also create variables without. any value for future use. for example : 
+
+let accountState;
+
+
+// console print methods 
+
+// console.log()  // Prints single values
+
+console.log(accountId);
+
+// console.table([]) // acts as an array use to print multiple values in a tabular form.
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
